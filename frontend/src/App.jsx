3ee70b2725c7ef8f66e5d0e27b60e5c6ea069dbf
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-12 03:11:36 +0530

    refactor form layout and button styling in video converter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
         Video Convertor
       </h1>
 
-      <form className="bg-amber-300 p-5 w-full h-[300px] rounded-xl mt-6 shadow-2xl drop-shadow-2xl flex flex-col justify-center items-center  gap-4">
+      <form className="bg-amber-300 p-5 w-3/5 h-[50vh] rounded-xl mt-6 shadow-2xl drop-shadow-2xl flex flex-col justify-center items-center  gap-4">
         <h2 className="text-3xl font-semibold text-amber-900">Select Video</h2>
 
         <div className="flex rounded-md drop-shadow-md ">
@@ -30,7 +30,7 @@ const App = () => {
             <option value="wav">WAV</option>
           </select>
         </div>
-        <button className="w-fit bg-amber-900 text-amber-100 px-4 py-2 rounded-lg">
+        <button className="w-fit bg-amber-700 text-amber-100 px-4 py-2 rounded-lg cursor-pointer hover:bg-amber-800">
           Convert
         </button>
       </form>
