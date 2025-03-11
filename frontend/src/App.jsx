be03b94adbe943cd06aad1c318b5ce657da06240
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-12 02:57:13 +0530

    enhance video converter UI with improved layout and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,18 +6,33 @@ const App = () => {
       <h1 className="text-8xl font-semibold text-amber-900 ">
         Video Convertor
       </h1>
-      <form className="bg-amber-300 p-5 rounded-xl mt-6 shadow-md drop-shadow-2xl">
-        <input type="file" name="video" id="video" accept="video/*" />
-        <select name="format" id="format">
-          <option value="mp4">MP4</option>
-          <option value="mkv">MKV</option>
-          <option value="webm">Webm</option>
-          <option value="mp3">MP3</option>
-          <option value="m4a">M4A</option>
-          <option value="flac">FLAC</option>
-          <option value="ogg">OGG</option>
-          <option value="wav">WAV</option>
-        </select>
+
+      <form className="bg-amber-300 p-5 w-full h-[300px] rounded-xl mt-6 shadow-2xl drop-shadow-2xl flex flex-col justify-center items-center  gap-4">
+        <h2 className="text-3xl font-semibold text-amber-900">Select Video</h2>
+
+        <div className="flex rounded-md drop-shadow-md ">
+          <input
+            type="file"
+            name="video"
+            id="video"
+            className="drop-shadow-md cursor-pointer bg-amber-200 p-4 rounded-l-lg   "
+            accept="video/*"
+          />
+
+          <select name="format" id="format" className="bg-amber-400 p-4 rounded-lg cursor-pointer hover:bg-amber-300">
+            <option value="mp4">MP4</option>
+            <option value="mkv">MKV</option>
+            <option value="webm">Webm</option>
+            <option value="mp3">MP3</option>
+            <option value="m4a">M4A</option>
+            <option value="flac">FLAC</option>
+            <option value="ogg">OGG</option>
+            <option value="wav">WAV</option>
+          </select>
+        </div>
+        <button className="w-fit bg-amber-900 text-amber-100 px-4 py-2 rounded-lg">
+          Convert
+        </button>
       </form>
     </div>
   );
