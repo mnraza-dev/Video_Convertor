3af99a23be2e10540b64cf23a5c2f03c69ce6bcf
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-12 21:24:44 +0530

    update App component title and button text for clarity; enhance format selection options

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-
 const App = () => {
   return (
     <div className="bg-amber-100 min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-8xl font-semibold text-amber-900 ">
-        Video Convertor
+        Video Convertor App
       </h1>
 
       <form className="bg-amber-300 p-5 w-3/5 h-[50vh] rounded-xl mt-6 shadow-2xl drop-shadow-2xl flex flex-col justify-center items-center  gap-4">
@@ -19,7 +18,11 @@ const App = () => {
             accept="video/*"
           />
 
-          <select name="format" id="format" className="bg-amber-400 p-4 rounded-lg cursor-pointer hover:bg-amber-300">
+          <select
+            name="format"
+            id="format"
+            className="bg-amber-400 p-4 rounded-lg cursor-pointer hover:bg-amber-300"
+          >
             <option value="mp4">MP4</option>
             <option value="mkv">MKV</option>
             <option value="webm">Webm</option>
@@ -28,10 +31,11 @@ const App = () => {
             <option value="flac">FLAC</option>
             <option value="ogg">OGG</option>
             <option value="wav">WAV</option>
+            <option value="mav">MAV</option>
           </select>
         </div>
         <button className="w-fit bg-amber-700 text-amber-100 px-4 py-2 rounded-lg cursor-pointer hover:bg-amber-800">
-          Convert
+          Convert Video
         </button>
       </form>
     </div>
